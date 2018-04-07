cd99bca8fe0fb2a82080cb72078186d5cc32a3a8
Chris Henry <cdhenry@gmail.com>
2018-04-07 15:52:30 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
     @user = User.find_or_create_by(uid: auth['uid']) do |u|
       u.name = auth['info']['name']
       u.email = auth['info']['email']
-      u.uid = auth['uid']
     end
 
     session[:user_id] = @user.id
